96a65c4e4613ff64ef1f05a162b456fc04252ccb
Alex Yang <aleozlx@gmail.com>
2017-10-10 19:50:39 -0500

    refactor

--- homography.py
+++ homography.py
@@ -9,7 +9,6 @@ def dlt(X, Y):
         [-y[2], 0, y[0]],
         [-y[1], y[0], 0]
     ]), x.T)
-
     choices = np.random.permutation(X.shape[1])[:4]
     A = np.vstack([homography_kron(x, y) for x,y in zip(X[:, choices].T, Y[:, choices].T)])
     V = np.linalg.svd(A)[2].T
@@ -31,29 +30,19 @@ def homography(src, dst):
 #     H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 #     return H, mask.ravel().tolist()
 
-def main():
-    # SIFT+FLANN matching
-    sift = cv2.xfeatures2d.SIFT_create()
-    (kpsA, desA) = sift.detectAndCompute(imA, None)
-    (kpsB, desB) = sift.detectAndCompute(imB, None)
-    flann = cv2.FlannBasedMatcher(dict(algorithm = 0, trees = 5), dict(checks = 50))
-    # Lowe's ratio test
-    matches = [m for m,n in flann.knnMatch(desA,desB,k=2) if m.distance < 0.7*n.distance] 
-    if len(matches)>10:
-        while 1:
-            try:
-                H, inliers = homography(
-                    np.float32([ kpsA[m.queryIdx].pt for m in matches ]),
-                    np.float32([ kpsB[m.trainIdx].pt for m in matches ]))
-                break
-            except numpy.linalg.linalg.LinAlgError:
-                continue
-        h,w = imA.shape
-        transformed_box = cv2.perspectiveTransform(np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2),H)
-        # cv2.polylines(imB,[np.int32(transformed_box)],True,255,3,cv2.LINE_AA)
-    else:
-        print("Not enough matches are found")
-        sys.exit(1)
+def plot_matching(imA,kpsA,imB,kpsB,matches,inliers):
+    im = cv2.drawMatches(imA,kpsA,imB,kpsB,matches,None,
+        matchColor = (0,255,0), # draw matches in green color
+        singlePointColor = None,
+        matchesMask = inliers, # draw only inliers
+        flags = 2)
+    plt.figure(figsize=(20,10))
+    plt.imshow(im, 'gray')
+
+def plot_stitching(imA,imB,H):
+    # Transform imA bounding box
+    h,w = imA.shape
+    transformed_box = cv2.perspectiveTransform(np.float32([[0,0],[0,h-1],[w-1,h-1],[w-1,0]]).reshape(-1,1,2),H)
     # Find bounding box
     bounding_box = np.array([
         min(0, *transformed_box[:,0,1]),              # [0,0] top
@@ -69,26 +58,40 @@ def main():
     # Copy imB onto imC
     imC[box_imB[0,0]:box_imB[0,0]+imB.shape[0],box_imB[1,0]:box_imB[1,0]+imB.shape[1]] = imB
     # Copy imA onto imC
+    H = H.copy()
     H[:2,2] += np.squeeze(translate_transform)[::-1] # adjust translation to imC
     _imA = cv2.warpPerspective(imA,H,imC.shape[::-1])
     imC[_imA!=0]=_imA[_imA!=0]
     # print(shape_imC)
-    
-
-    # Plot transformed bounding box and matching points
-    # imC = cv2.drawMatches(imA,kpsA,imB,kpsB,matches,None,
-    #     matchColor = (0,255,0), # draw matches in green color
-    #     singlePointColor = None,
-    #     matchesMask = matchesMask, # draw only inliers
-    #     flags = 2)
     plt.figure(figsize=(20,10))
     plt.imshow(imC, 'gray')
-    # plt.imshow(cv2.warpPerspective(imA,H,imC.shape[::-1]), 'gray')
-    plt.show()
 
-
-
-if __name__ == '__main__':
+def main():
+    # Read images
     imA = cv2.cvtColor(cv2.imread("A.png"), cv2.COLOR_BGR2GRAY)
     imB = cv2.cvtColor(cv2.imread("B1.png"), cv2.COLOR_BGR2GRAY)
+    # SIFT+FLANN matching
+    sift = cv2.xfeatures2d.SIFT_create()
+    (kpsA, desA) = sift.detectAndCompute(imA, None)
+    (kpsB, desB) = sift.detectAndCompute(imB, None)
+    flann = cv2.FlannBasedMatcher(dict(algorithm = 0, trees = 5), dict(checks = 50))
+    # Lowe's ratio test
+    matches = [m for m,n in flann.knnMatch(desA,desB,k=2) if m.distance < 0.7*n.distance] 
+    if len(matches)>10:
+        while 1: # Just try again when getting singular matrix
+            try:
+                H, inliers = homography(
+                    np.float32([ kpsA[m.queryIdx].pt for m in matches ]),
+                    np.float32([ kpsB[m.trainIdx].pt for m in matches ]))
+                break
+            except np.linalg.linalg.LinAlgError:
+                continue
+    else:
+        print("Not enough matches are found")
+        sys.exit(1)
+    plot_matching(imA,kpsA,imB,kpsB,matches,inliers)
+    plot_stitching(imA,imB,H)
+    plt.show()
+
+if __name__ == '__main__':
     main()
